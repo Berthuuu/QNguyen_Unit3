5341b8db02ed66eb4b2010f1c9141ca594bdd5a5
Berthuuu <quynhdn@yahoo.com>
2022-03-18 02:00:07 -0700

    Finished lesson A

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rbPlayer;
+    public float gravityModifier;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        rbPlayer = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+   }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool spaceDown = Input.GetKeyDown(KeyCode.Space);
+        if (spaceDown)
+        {
+            rbPlayer.AddForce(Vector3.up * 10, ForceMode.Impulse);
+
+        }
     }
 }
